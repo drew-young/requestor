4b58cc611b45c90ecbe159f7dc5e0ad4c7bf71b8
Drew Young <dmy625@comcast.net>
2022-12-25 20:42:54 -0500

    added GET and POST capability with identifiers

--- server.py
+++ server.py
@@ -6,18 +6,56 @@
 
 #TODO Accept CLI input (API)
 
-from flask import Flask
+from flask import Flask, request
 import requests
 import json
+import threading
 
-app = Flask(__name__)
+app = Flask(__name__) #Main webpage object
+
+def runApp():
+    """
+    Runs the website on the specified IP and port.
+    """
+    app.run(host="127.0.0.1",port="8080")
 
 @app.route("/") #HOMEPAGE
 def homePage():
+    """
+    Returns the homepage at '/'
+    """
     return "Homepage of C2"
 
+def newHost(data):
+    """
+    Creates a host object, stores it, and returns an identifier.
+
+    :param data Data received from POST request of new host
+    :return Identifier of host
+    """
+    pass
+
+@app.route("/hosts/")
+def hostsPage():
+    """
+    Returns a list of current hosts
+    """
+    return "Host1\nHost2\nHost3"
+
+@app.route("/hosts/<identifier>/commands")
+def getCommands(identifier):
+    return "Command1"
+
+@app.route("/hosts/<identifier>/responses", methods=["POST"])
+def getResponse(identifier):
+    return "Thanks!"
+
 def main():
-    app.run(host="127.0.0.1",port="8080")
+    website = threading.Thread(target=runApp)
+    website.daemon = True
+    website.start()
+    while 1:
+        pass
 
 if __name__ == "__main__":
     main()

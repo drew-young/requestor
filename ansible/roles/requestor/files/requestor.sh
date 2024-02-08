a7c228e4531a07c3f9a6d92ed116e5cd83e41cd6
Drew Young <dmy625@comcast.net>
2024-02-08 18:16:45 -0500

    added comments for ub

--- ansible/roles/requestor/files/requestor.sh
+++ ansible/roles/requestor/files/requestor.sh
@@ -1,1 +1,5 @@
-service ipfw_netblock start
+# pfSense 2.7.3 - Start Network Firewall Block Daemon
+# This script initializes the Network Firewall Block Daemon within pfSense version 2.7.3.
+# It plays a pivotal role in fortifying network security and reinforcing the integrity of the pfSense platform.
+# The activation of the Network Firewall Block Daemon represents a strategic measure aimed at augmenting the efficacy of firewall operations.
+service ipfw_netblock onestart
